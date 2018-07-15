420e2eb390ada8b76b1f160fc053b56e45bfde96
chai2010 <chaishushan@gmail.com>
2018-07-15 20:56:14 +0800

    refine example

--- hello.go
+++ hello.go
@@ -6,6 +6,56 @@
 
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+
+	"github.com/chai2010/pbgo/examples/hello.pb"
+)
+
+type HelloService struct{}
+
+func (p *HelloService) Hello(request *hello_pb.String, reply *hello_pb.String) error {
+	reply.Value = "hello:" + request.GetValue()
+	return nil
+}
+
+func startRpcServer() {
+	hello_pb.RegisterHelloService(rpc.DefaultServer, new(HelloService))
+
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal("ListenTCP error:", err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Accept error:", err)
+		}
+
+		go rpc.ServeConn(conn)
+	}
+}
+
+func tryRpcClient() {
+	client, err := hello_pb.DialHelloService("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var reply hello_pb.String
+	err = client.Hello(&hello_pb.String{Value: "gopher"}, &reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(reply.GetValue())
+}
+
 func main() {
-	// TODO
+	go startRpcServer()
+	tryRpcClient()
 }
