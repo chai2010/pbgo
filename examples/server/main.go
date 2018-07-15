f9a1b7ab299623fd768d00538b0d75ab9964943a
chai2010 <chaishushan@gmail.com>
2018-07-15 20:16:54 +0800

    refine code

---
+++ examples/server/main.go
@@ -0,0 +1,16 @@
+// Copyright 2018 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/chai2010/pbgo/examples/hello.pb"
+)
+
+func main() {
+	var x hello_pb.String
+	fmt.Println(x)
+}
