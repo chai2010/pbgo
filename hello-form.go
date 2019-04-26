77117f3c067745657c2eec38b428c998eaa06460
chai2010 <chaishushan@gmail.com>
2019-04-26 11:07:48 +0800

    refine form example

--- hello-form.go
+++ hello-form.go
@@ -7,7 +7,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -20,22 +19,40 @@ import (
 func main() {
 	router := httprouter.New()
 
-	// curl -d "user=chai&email=11@qq.com&text=hello&next=http://github.com" localhost:8080/api/comment-form
-	router.POST("/api/comment-form", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	// http://localhost:8080
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		w.Header().Add("Content-Type", "text/html; charset=utf-8")
+
+		fmt.Fprint(w, `
+			<form method="POST" action="/api/comment">
+				<input type="text" name="user" value="user name">
+				<input type="email" name="email" value="123@gmail.com">
+				<input name="url" value="http://chai2010.cn"></textarea>
+				<textarea name="text" value="text">www</textarea>
+				<input type="text" name="next" value="/thanks">
+				<button type="submit">Send Test</button>
+			</form>
+		`)
+	})
+
+	router.GET("/thanks", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		fmt.Fprintln(w, "Thanks")
+	})
+
+	// curl -d "user=chai&email=11@qq.com&text=hello&next=http://github.com" localhost:8080/api/comment
+	router.POST("/api/comment", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		var form form_pb.Comment
 		if err := pbgo.BindForm(r, &form); err != nil {
 			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-			log.Println(err)
+			log.Println("err:", err)
 			return
 		}
-
-		s, _ := json.Marshal(form)
-		fmt.Fprintln(w, string(s))
 
 		if form.Next != "" {
 			http.Redirect(w, r, form.Next, http.StatusFound)
-			return
 		}
+
+		log.Println("form:", form)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", router))
