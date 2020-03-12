0726146fb04b82945f698724066555b8eaf60429
chai2010 <chaishushan@gmail.com>
2020-03-12 11:40:28 +0800

    plugin: add grpc build tag

---
+++ protoc-gen-pbgo/link_grpc.go
@@ -0,0 +1,10 @@
+// Copyright 2018 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build grpc
+// go build -tags grpc
+
+package main
+
+import _ "github.com/golang/protobuf/protoc-gen-go/grpc"
